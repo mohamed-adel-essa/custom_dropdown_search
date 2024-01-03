7b8ff9cc03982cb516c27f8ce0e8e1b1ffd2c075
mohamed Adel <mohadel1033@gmail.com>
2024-01-03 17:43:02 +0200

    init

---
+++ lib/src/properties/favorite_item_props.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../custom_dropdown_search.dart';
+
+ 
+
+class FavoriteItemProps<T> {
+  ///show or hide favorites items
+  final bool showFavoriteItems;
+
+  ///to customize favorites chips
+  final FavoriteItemsBuilder<T>? favoriteItemBuilder;
+
+  ///favorites items list
+  final FavoriteItems<T>? favoriteItems;
+
+  ///favorite items alignment
+  final MainAxisAlignment favoriteItemsAlignment;
+
+  const FavoriteItemProps({
+    this.favoriteItemBuilder,
+    this.favoriteItems,
+    this.favoriteItemsAlignment = MainAxisAlignment.start,
+    this.showFavoriteItems = false,
+  });
+}
